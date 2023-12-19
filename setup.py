11d00496a8e517ac3adbd9fa44f6d774932dc0c6
chaza011 <118681555+chaza011@users.noreply.github.com>
2023-12-19 11:39:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='CustomFormers',
     version='0.1',
+    url = 'https://github.com/chaza011/CustomFormers',
     packages=find_packages(),
     install_requires=[
         'torch>=1.13',
-    ],
+    ]
 )
 
 
